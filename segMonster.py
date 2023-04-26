5043d2dab0525c4a6f88b0145d8c77e81b36c3a6
Max Brüggemann <mail@maxbrueggemann.de>
2023-04-26 22:48:54 +0200

    Fix bug in sendData

--- segMonster.py
+++ segMonster.py
@@ -117,10 +117,7 @@ gammaTable=[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8,
 
 def sendData(rawdata):
     global sock
-    digSum=0
     outdata=[]
-    for x in rawdata:
-        digSum+=x
     for pos in rawdata:
         outdata.append(gammaTable[int(pos)])
     MESSAGE=b"D"+bytes(outdata)
